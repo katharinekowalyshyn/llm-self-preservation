9b95583bb29cf74856d7389cd2e6ec65e041ebf7
Thies Oelerich <thies.oelerich@tuwien.ac.at>
2025-04-10 16:10:14 +0200

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "LLMSP";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        shellHook = ''
+          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+            with pkgs;
+            lib.makeLibraryPath [
+              xorg.libX11
+              xorg.libXt
+              xorg.libSM
+              zlib
+              glib
+              udev
+              libGL
+              glfw
+              boost
+              gmp
+            ]
+          }:${pkgs.stdenv.cc.cc.lib}/lib"
+            source .venv/bin/activate
+            export PYTHONPATH="$PYTHONPATH:$VIRTUAL_ENV/lib/python3.11/site-packages"
+        '';
+        name = "LLMSP";
+        buildInputs = with pkgs; [
+          python311Packages.pip
+          python311Packages.tkinter
+          python311Packages.cmake
+          cmake
+          gcc
+          stdenv
+          udev
+          libGL
+          glfw
+          graphviz
+          gmp
+          #glibc.static
+        ];
+      };
+    };
+}
